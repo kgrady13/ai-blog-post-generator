993c8ee8bc0654831f676668bfa3f8452361a271
Karson Grady <karson@massivelyhuman.com>
2024-08-11 07:13:20 -0700

    redeploy

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Lab-AI",
+  description: "An AI-powered blog generator by Launching Lab.",
+  mainNav: [],
+  links: {
+    twitter: "https://twitter.com/karsongrady",
+    github: "https://github.com/kgrady13/ai-blog-post-generator",
+    website: "https://launchinglab.com",
+  },
+};
